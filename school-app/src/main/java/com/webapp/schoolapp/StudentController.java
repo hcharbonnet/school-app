9189d2c4b3795d68cb76ec5c3961320c2ebe7d8a
Helen Charbonnet <helen.charbonnet@afs.com>
2023-09-20 12:11:48 -0400

    get method working

--- school-app/src/main/java/com/webapp/schoolapp/StudentController.java
+++ school-app/src/main/java/com/webapp/schoolapp/StudentController.java
@@ -3,11 +3,10 @@ package com.webapp.schoolapp;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.util.UriComponents;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -30,4 +29,11 @@ public class StudentController {
 		
 		return new ResponseEntity<Void>(headers,HttpStatus.CREATED);
 	}
+	
+	@GetMapping("/")
+	public ResponseEntity<List<Student>> getAllStudents() {
+		return ResponseEntity.ok(studentService.getAllStudents());
+	}
+	
+
 }
--- school-app/src/main/java/com/webapp/schoolapp/StudentService.java
+++ school-app/src/main/java/com/webapp/schoolapp/StudentService.java
@@ -1,5 +1,8 @@
 package com.webapp.schoolapp;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +12,14 @@ public class StudentService {
 	private StudentRepository studentRepository;
 	
 	public Long createNewStudent(Student student) {
-		
 		student = studentRepository.save(student);
 		return student.getId();
 	}
+	
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+	
+
 
 }
